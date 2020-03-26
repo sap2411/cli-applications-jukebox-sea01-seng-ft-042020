37be13282256ac5249c7853cd57dc60cc0f7e599
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-26 03:21:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,8 +28,10 @@ def play(songs)
   puts "Please enter a song name or number:"
   reply = gets.strip
   songs
-  if songs.include? reply
-    puts "Playing #{reply}"
+  if (1..9).to_a.index(input.to_i) != nil
+    puts "Playing #{my_songs[input.to_i - 1]}"
+  elsif my_songs.index(input) != nil
+    puts "Playing #{input}"
   else
     puts "Invalid input, please try again"
   end
